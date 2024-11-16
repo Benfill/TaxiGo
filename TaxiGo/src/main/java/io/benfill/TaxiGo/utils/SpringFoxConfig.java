06ac29215594b43d912e1ae8c1e62f8ffccbc43b
Anass Benfillous <anasse.benfillous@gmail.com>
2024-11-16 19:29:04 +0100

    fix swagger remove openapi

--- TaxiGo/src/main/java/io/benfill/TaxiGo/utils/SpringFoxConfig.java
+++ TaxiGo/src/main/java/io/benfill/TaxiGo/utils/SpringFoxConfig.java
@@ -11,13 +11,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {                                    
-    @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("io.benfill.TaxiGo")) 
-          .paths(PathSelectors.any())                          
-          .build();                                           
-    }
+public class SpringFoxConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("io.benfill.TaxiGo")).paths(PathSelectors.any()).build();
+	}
 }
